e012f331af11cbfad2b57ab4c45759d6d8832b70
Léo Moncoiffet <127783517+Helfhris@users.noreply.github.com>
2024-12-27 06:11:16 -0500

    Exercise 2 Start Steps 1-3 Done

--- Happy_Birthday_Ex1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Happy_Birthday_Ex1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
+                    //GreetingImage("Happy Birthday Léo", from = "From Emma", modifier = Modifier.padding(8.dp))
                     GreetingText("Happy Birthday Léo", from = "From Emma", modifier = Modifier.padding(8.dp))
                 }
             }
@@ -68,10 +71,20 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     MyApplicationTheme {
-        GreetingText(message = "Happy Birthday Léo", from = "From Emma")
+        GreetingImage(message = "Happy Birthday Léo", from = "From Emma")
+        //GreetingText(message = "Happy Birthday Léo", from = "From Emma")
     }
 }
