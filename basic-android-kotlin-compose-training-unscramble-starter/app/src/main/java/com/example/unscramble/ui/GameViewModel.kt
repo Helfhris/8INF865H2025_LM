66a1b781892ada494cc3e1f9e7698d55e8996f3a
Léo Moncoiffet <127783517+Helfhris@users.noreply.github.com>
2025-02-05 04:50:49 -0500

    Step 6

---
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.unscramble.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.example.unscramble.data.allWords
+
+private lateinit var currentWord: String
+private var usedWords: MutableSet<String> = mutableSetOf()
+private val _uiState = MutableStateFlow(GameUiState())
+
+
+class GameViewModel : ViewModel() {
+
+    init{
+        resetGame()
+    }
+
+    var userGuess by mutableStateOf("")
+        private set
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+
+
+    private fun pickRandomWordandShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordandShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordandShuffle())
+    }
+
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
+    }
+}
